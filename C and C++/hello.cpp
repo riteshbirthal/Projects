90410c8d48f2d34d2b593c1840e74191d0a6d8ca
riteshbirthal <riteshbirthal@gmail.com>
2024-08-25 19:04:12 +0530

    Changed chat application name

--- C and C++/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "Learning C++";
-	return 0;
-}
--- Python/PyTest/src/temp2.py
+++
@@ -1,5 +0,0 @@
-from build.temp3 import *
-def func2():
-    return hello()
-
-print(func2())
